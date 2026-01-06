1607f10cad975b1cdb58b81039cc3b151ed380b2
Sweety3001 <sweetylog19@gmail.com>
2026-01-06 01:28:53 +0000

    counter

---
+++ counter/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+/*
+function App() {
+ const [count, setCount] = useState(10)
+ return(
+  <>
+    <h1>Counter Value: {count}</h1>
+    <button onClick={() => setCount(count + 1)}>Add Value {count}</button>
+    <br />
+    <button onClick={() => setCount(count - 1)}>Remove Value {count}</button>
+  </>
+ )
+}
+*/
+
+function App() {
+  const[count, setCount]=useState(0);
+  const addValue=()=>{
+    if(count<20){
+      setCount(count+1)
+    }
+  }
+  const removeValue=()=>{
+    if(count>0){
+      setCount(count-1)
+    }
+  }
+  return(
+    <>
+      <h1>Counter Value: {count}</h1>
+      <button onClick={addValue}>Add Value {count}</button>
+      <br />
+      <button onClick={removeValue}>Remove Value {count}</button>
+    </>
+  )
+}
+
+// function App() {
+
+//   let counter =5;
+
+//   const addValue = () => {
+//     counter += 1;
+//     console.log(counter);
+//   }
+
+//   return (
+//     <>
+//     <h1>Counter Value: {counter}</h1>
+//     <button onClick={addValue}>Add Value</button>
+//     <br />
+//     <button>Remove Value</button>
+//     </>
+//   )
+// }
+/* what is wrong with the above code?
+The counter variable is not part of the component's state, so changes to it will not trigger a re-render of the component. As a result, the displayed counter value will not update when the addValue function is called.
+To fix this, we should use the useState hook to manage the counter state.
+*/
+
+
+export default App
